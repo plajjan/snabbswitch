210f029abe75c8a8ad70ca0bff9c235f15089a0c
Kristian Larsson <kristian@spritelink.net>
2014-12-01 17:04:23 +0100

    mini poc

--- src/apps/ddos/poc.lua
+++ src/apps/ddos/poc.lua
@@ -1,32 +1,16 @@
 module(..., package.seeall)
 
-local app = require("core.app")
-local buffer = require("core.buffer")
-local datagram = require("lib.protocol.datagram")
-local ffi = require("ffi")
-local filter = require("lib.pcap.filter")
-local ipv4 = require("lib.protocol.ipv4")
-local lib = require("core.lib")
 local link = require("core.link")
-local packet = require("core.packet")
-
 
 PoC = {}
 
 function PoC:new (arg)
-   local conf = arg and config.parse_app_arg(arg) or {}
-   local o = {
-   }
-   self = setmetatable(o, {__index = PoC})
-   return self
+   return setmetatable({}, {__index = PoC})
 end
 
 function PoC:push()
-   local i = assert(self.input.input, "input port not found")
-   local o = assert(self.output.output, "output port not found")
-   while not link.empty(i) and not link.full(o) do
-      local p = link.receive(i)
-	  link.transmit(o, p)
---	  return
+   while not link.empty(self.input.input) and not link.full(self.output.output) do
+	  link.transmit(self.output.output, link.receive(self.input.input))
+	  return
    end
 end
