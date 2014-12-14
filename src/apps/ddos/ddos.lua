a87d2fae2e2ce84a468c8bd8eda92f93020d5434
Kristian Larsson <kristian@spritelink.net>
2014-12-14 20:45:04 +0100

    Periodically clean up blacklist
    .. and remove most traces of the old blocklist.

--- src/apps/ddos/ddos.lua
+++ src/apps/ddos/ddos.lua
@@ -22,7 +22,6 @@ function DDoS:new (arg)
    assert(conf.block_period >= 5, "block_period must be at least 5 seconds")
    local o =
    {
-      blocklist = {},
       blacklist = {
          ipv4 = {},
          ipv6 = {}
@@ -61,12 +60,19 @@ end
 
 
 function DDoS:periodic()
-   for src_ip, blocklist in pairs(self.blocklist) do
-      if blocklist.block_until < tonumber(app.now()) then
-         self.blocklist[src_ip] = nil
+   -- unblock old entries in blacklist
+   for src_ip, ble in pairs(self.blacklist.ipv4) do
+      if ble.block_until < tonumber(app.now()) then
+         self.blacklist.ipv4[src_ip] = nil
+      end
+   end
+   for src_ip, ble in pairs(self.blacklist.ipv6) do
+      if ble.block_until < tonumber(app.now()) then
+         self.blacklist.ipv6[src_ip] = nil
       end
    end
 
+   -- and in rules
    for rule_name,rule in pairs(self.rules) do
       for src_ip,src_info in pairs(rule.srcs) do
          if src_info.block_until ~= nil and src_info.block_until < tonumber(app.now()) then
@@ -199,9 +205,9 @@ function DDoS:report()
    print("Rx: " .. s_i.rxpackets .. " packets / " .. s_i.rxbytes .. " bytes")
    print("Tx: " .. s_o.txpackets .. " packets / " .. s_o.txbytes .. " bytes / " .. s_o.txdrop .. " packet drops")
    print("Configured block period: " .. self.block_period .. " seconds")
-   print("Block list:")
-   for src_ip,blocklist in pairs(self.blocklist) do
-      print("  " .. src_ip .. " blocked for another " .. string.format("%0.1f", tostring(blocklist.block_until - tonumber(app.now()))) .. " seconds")
+   print("Blacklist:")
+   for src_ip,ble in pairs(self.blacklist.ipv4) do
+      print("  " .. src_ip .. " blocked for another " .. string.format("%0.1f", tostring(ble.block_until - tonumber(app.now()))) .. " seconds")
    end
 
    print("Traffic rules:")
@@ -213,9 +219,9 @@ function DDoS:report()
          pps_tokens = string.format("%5s", "-")
          -- if source is in blocklist it means we shortcut and thus don't
          -- calculate pps, so we write '-'
-         if self.blocklist[src_ip] == nil and src_info.pps_tokens then
-            pps_tokens = string.format("%5.0f", src_info.pps_tokens )
-         end
+--         if self.blocklist[src_ip] == nil and src_info.pps_tokens then
+--            pps_tokens = string.format("%5.0f", src_info.pps_tokens )
+--         end
          str = string.format("  %15s last: %d tokens: %s ", src_ip, tonumber(app.now())-src_info.last_time, pps_tokens)
          if src_info.block_until == nil then
             str = string.format("%s %-7s", str, "allowed")
@@ -225,17 +231,6 @@ function DDoS:report()
          print(str)
       end
    end
-end
-
-
--- return statistics snapshot
-function DDoS:get_stat_snapshot ()
-   return
-   {
-      rxpackets = self.input.input.stats.txpackets,
-      txpackets = self.output.output.stats.txpackets,
-      time = tonumber(C.get_time_ns()),
-   }
 end
 
 
@@ -329,7 +324,7 @@ function test_performance()
    local c = config.new()
    config.app(c, "source", pcap.PcapReader, "apps/ddos/selftest.cap.input")
    config.app(c, "repeater", basic_apps.FastRepeater)
-   config.app(c, "ddos", DDoS, { rules = rules, block_period = 60 })
+   config.app(c, "ddos", DDoS, { rules = rules, block_period = 10 })
    config.app(c, "sink", basic_apps.FastSink)
    config.link(c, "source.output -> repeater.input")
    config.link(c, "repeater.output -> ddos.input")
@@ -338,13 +333,22 @@ function test_performance()
 
    local ddos_app = app.app_table.ddos
 
-   local seconds_to_run = 5
-   print("== Perf test - dropping NTP by match")
+   timer.activate(timer.new(
+      "report",
+      function()
+          app.app_table.ddos:report()
+      end,
+      1e9,
+      'repeating'
+   ))
+
+   local seconds_to_run = 30
+   print("== Perf test - dropping NTP by match!")
    app.main({duration = seconds_to_run})
 
    print("source sent: " .. app.app_table.source.output.output.stats.txpackets)
    print("repeater sent: " .. app.app_table.repeater.output.output.stats.txpackets)
    print("sink received: " .. app.app_table.sink.input.input.stats.rxpackets)
-   print("Effective rate: " .. tostring(app.app_table.repeater.output.output.stats.txpackets / seconds_to_run))
+   print("Effective rate: " .. string.format("%0.1f", tostring(app.app_table.repeater.output.output.stats.txpackets / seconds_to_run)))
    return true
 end
