3d299de2123098641d0a0c46687442169605605a
Kristian Larsson <kristian@spritelink.net>
2016-03-29 13:17:41 +0200

    Support VLAN tagging on clean interface
    Fixes #3.

--- src/program/snabbddos/snabbddos.lua
+++ src/program/snabbddos/snabbddos.lua
@@ -10,9 +10,10 @@ local ipv4      = require("lib.protocol.ipv4")
 local json      = require("lib.json")
 local lib       = require("core.lib")
 local main      = require("core.main")
-local Tap       = require("apps.tap.tap").Tap
 
+local Tap       = require("apps.tap.tap").Tap
 local ddos      = require("apps.ddos.ddos")
+local vlan      = require("apps.vlan.vlan")
 
 local usage = require("program.snabbddos.README_inc")
 
@@ -27,15 +28,17 @@ function run (args)
    local intf_dirty, intf_clean
    local config_file_path
    local report = false
+   local vlan_tag = false
 
    -- argument parsing
    local opt = {}
    function opt.h (arg) print(usage) main.exit(1) end
    function opt.c (arg) config_file_path = arg end
+   function opt.r (arg) report = true end
    function opt.C (arg) intf_clean = arg end
    function opt.D (arg) intf_dirty = arg end
-   function opt.r (arg) report = true end
-   args = lib.dogetopt(args, opt, "hc:rD:C:", long_opts)
+   function opt.V (arg) vlan_tag = arg end
+   args = lib.dogetopt(args, opt, "hc:rD:C:V:", long_opts)
 
    local c = config.new()
 
@@ -66,7 +69,14 @@ function run (args)
 
    config.app(c, "ddos", ddos.DDoS, { mitigations = mitigation_config })
    config.link(c, "dirty.output -> ddos.input")
-   config.link(c, "ddos.output -> clean.input")
+   if vlan_tag then
+      print("Using VLAN tag: " .. vlan_tag)
+      config.app(c, "vlan_tagger", vlan.Tagger, { tag = vlan_tag })
+      config.link(c, "ddos.output -> vlan_tagger.input")
+      config.link(c, "vlan_tagger.output -> clean.input")
+   else
+      config.link(c, "ddos.output -> clean.input")
+   end
    engine.configure(c)
    engine.main()
 end
