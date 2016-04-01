0c3c9504113ea97f2a2fedfd47b02cc9694a81c6
Kristian Larsson <kristian@spritelink.net>
2016-04-01 15:19:51 +0200

    Separate argument parsing + sanity checks

--- src/program/snabbddos/snabbddos.lua
+++ src/program/snabbddos/snabbddos.lua
@@ -1,5 +1,6 @@
 module(..., package.seeall)
 
+local S         = require("syscall")
 local engine    = require("core.app")
 local config    = require("core.config")
 local timer     = require("core.timer")
@@ -20,32 +21,72 @@ local long_opts = {
    help         = "h",
    mconfig      = "m",
    clean        = "C",
-   dirty        = "D"
+   dirty        = "D",
+   vlan         = "V"
 }
 
-function run (args)
-   local intf_dirty, intf_clean
-   local mconfig_file_path
-   local report = false
-   local vlan_tag = false
+local function fatal(msg)
+   print('error: '..msg)
+   main.exit(1)
+end
 
+
+local function file_exists(path)
+   local stat = S.stat(path)
+   return stat and stat.isreg
+end
+
+local function dir_exists(path)
+   local stat = S.stat(path)
+   return stat and stat.isdir
+end
+
+local function nic_exists(pci_addr)
+   local devices="/sys/bus/pci/devices"
+   return dir_exists(("%s/%s"):format(devices, pci_addr)) or
+      dir_exists(("%s/0000:%s"):format(devices, pci_addr))
+end
+
+function parse_args(args)
    -- argument parsing
-   local opt = {}
-   function opt.h (arg) print(usage) main.exit(1) end
-   function opt.m (arg) mconfig_file_path = arg end
-   function opt.r (arg) report = true end
-   function opt.C (arg) intf_clean = arg end
-   function opt.D (arg) intf_dirty = arg end
-   function opt.V (arg) vlan_tag = arg end
-   args = lib.dogetopt(args, opt, "hm:rD:C:V:", long_opts)
+   local opt = {
+      report = false,
+      vlan = false
+   }
+   local handlers = {}
+   -- help
+   function handlers.h (arg) print(usage) main.exit(1) end
+   -- mitigation config
+   function handlers.m (arg) opt.mconfig_file_path = arg end
+   -- report
+   function handlers.r (arg) opt.report = true end
+   -- interface clean
+   function handlers.C (arg) opt.intf_clean = arg end
+   -- interface dirty
+   function handlers.D (arg)
+      opt.intf_dirty = arg
+   end
+   function handlers.V (arg) opt.vlan_tag = arg end
+   args = lib.dogetopt(args, handlers, "hm:rD:C:V:", long_opts)
+
+   if not opt.intf_clean then fatal("Missing argument -C") end
+   if not opt.intf_dirty then fatal("Missing argument -D") end
+   if not opt.mconfig_file_path then fatal("Missing argument -m") end
+
+   return opt
+end
+
+function run (args)
+   local opt = parse_args(args)
 
    local c = config.new()
 
    -- setup interfaces
-   config.app(c, "dirty", Tap, intf_dirty)
-   config.app(c, "clean", Tap, intf_clean)
+   config.app(c, "dirty", Tap, opt.intf_dirty)
+   config.app(c, "clean", Tap, opt.intf_clean)
 
-   if report then
+   -- report every second
+   if opt.report then
       timer.activate(timer.new(
          "report",
          function()
@@ -58,12 +99,12 @@ function run (args)
 
    -- TODO: we need the reverse path set up as well so we can reply to ARP
    -- packets but first we need the ARP app
-   config.app(c, "ddos", ddos.DDoS, { config_file_path = mconfig_file_path })
+   config.app(c, "ddos", ddos.DDoS, { config_file_path = opt.mconfig_file_path })
    config.link(c, "dirty.output -> ddos.input")
    -- if vlan_tag is set we tag all egress/clean packets with a VLAN tag
-   if vlan_tag then
-      print("Using VLAN tag: " .. vlan_tag)
-      config.app(c, "vlan_tagger", vlan.Tagger, { tag = vlan_tag })
+   if opt.vlan_tag then
+      print("Using VLAN tag: " .. opt.vlan_tag)
+      config.app(c, "vlan_tagger", vlan.Tagger, { tag = opt.vlan_tag })
       config.link(c, "ddos.output -> vlan_tagger.input")
       config.link(c, "vlan_tagger.output -> clean.input")
    else
