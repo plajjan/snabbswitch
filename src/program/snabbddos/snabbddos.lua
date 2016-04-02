186828f356628b7ed70c22c60c05e54778520d8d
Kristian Larsson <kristian@spritelink.net>
2016-04-02 14:33:25 +0200

    Fix 10G NIC + config of snabbddos program
    I've refactored the config of the snabbddos program. It's quite a bit
    lengthier but I think it's reasonably easy to read compared to a more
    compact version.
    
    With this change now also have support for 10G intel NICs.
    
    Fixes #8.

--- src/program/snabbddos/snabbddos.lua
+++ src/program/snabbddos/snabbddos.lua
@@ -1,12 +1,11 @@
 module(..., package.seeall)
 
+local Intel82599 = require("apps.intel.intel_app").Intel82599
 local S         = require("syscall")
 local engine    = require("core.app")
 local config    = require("core.config")
 local timer     = require("core.timer")
 local pci       = require("lib.hardware.pci")
-local intel10g  = require("apps.intel.intel10g")
-local intel_app = require("apps.intel.intel_app")
 local ipv4      = require("lib.protocol.ipv4")
 local lib       = require("core.lib")
 local main      = require("core.main")
@@ -51,7 +50,7 @@ function parse_args(args)
    -- argument parsing
    local opt = {
       report = false,
-      vlan = false
+      vlan_tag = false
    }
    local handlers = {}
    -- help
@@ -69,8 +68,11 @@ function parse_args(args)
    function handlers.V (arg) opt.vlan_tag = arg end
    args = lib.dogetopt(args, handlers, "hm:rD:C:V:", long_opts)
 
-   if not opt.intf_clean then fatal("Missing argument -C") end
    if not opt.intf_dirty then fatal("Missing argument -D") end
+   if not opt.intf_clean then
+      print("Clean interface not specified, assuming SnabbDDoS-on-a-stick")
+      opt.intf_clean = opt.intf_dirty
+   end
    if not opt.mconfig_file_path then fatal("Missing argument -m") end
 
    return opt
@@ -81,16 +83,106 @@ function run (args)
 
    local c = config.new()
 
-   -- setup interfaces
-   if nic_exists(opt.intf_dirty) then
+   -- TODO: we need the reverse path set up as well so we can reply to ARP
+   -- packets but first we need the ARP app
+
+   if opt.intf_clean == opt.intf_dirty then
+      -- same physical interface used for dirty and clean traffic which means we
+      -- use VLAN tag to put clean traffic on a separate logical interface and
+      -- send out on the physical "dirty" interface. We only tag clean traffic
+      -- since there is a performance penalty incurred by tagging and
+      -- statistically we will have less packets on the clean side
+
+      print("Using same physical interface for dirty and clean traffic")
+      -- if dirty and clean interface is the same, require vlan tagging
+      if not opt.vlan_tag then
+         fatal("VLAN id must be set to use same interface for dirty and clean traffic")
+      end
+
+      -- setup physical interface, 10G or tap
+      if nic_exists(opt.intf_dirty) then
+         config.app(c, "dirty", Intel82599, {
+            pciaddr=opt.intf_dirty,
+         })
+         -- input and output interface names
+         iif_name = "rx"
+         oif_name = "tx"
+
+         -- link apps, note "tx"/"rx" on intel 10G interface
+         config.link(c, "dirty.tx -> ddos.input")
+         config.link(c, "vlan_tagger.output -> dirty.rx")
+      else
+         print("dirty interface '" .. opt.intf_dirty .. "' is not an existing PCI device, assuming tap interface")
+         config.app(c, "dirty", Tap, opt.intf_dirty)
+
+         -- input and output interface names
+         iif_name = "input"
+         oif_name = "output"
+      end
+
+      config.app(c, "ddos", ddos.DDoS, { config_file_path = opt.mconfig_file_path })
+      -- clean interface is a logical vlan tagger that then goes out physical
+      -- dirty interface
+      config.app(c, "vlan_tagger", vlan.Tagger, { tag = opt.vlan_tag })
+
+      -- link apps
+      config.link(c, "dirty."..oif_name.." -> ddos.input")
+      config.link(c, "ddos.output -> vlan_tagger.input")
+      config.link(c, "vlan_tagger.output -> dirty."..iif_name)
+
    else
-      print("dirty interface '" .. opt.intf_dirty .. "' is not an existing PCI device, assuming tap interface")
-      config.app(c, "dirty", Tap, opt.intf_dirty)
-   end
-   if nic_exists(opt.intf_dirty) then
-   else
-      print("dirty interface '" .. opt.intf_dirty .. "' is not an existing PCI device, assuming tap interface")
-      config.app(c, "clean", Tap, opt.intf_clean)
+      -- different physical interfaces for dirty and clean traffic
+
+      config.app(c, "ddos", ddos.DDoS, { config_file_path = opt.mconfig_file_path })
+
+      -- setup physical dirty interface, 10G or tap
+      if nic_exists(opt.intf_dirty) then -- 10G
+         config.app(c, "dirty", Intel82599, {
+            pciaddr=opt.intf_dirty,
+         })
+
+         -- link dirty -> ddos
+         config.link(c, "dirty.tx -> ddos.input")
+
+      else -- tap
+         print("dirty interface '" .. opt.intf_dirty .. "' is not an existing PCI device, assuming tap interface")
+         config.app(c, "dirty", Tap, opt.intf_dirty)
+
+         -- link dirty -> ddos
+         config.link(c, "dirty.output -> ddos.input")
+      end
+
+      -- setup physical clean interface, 10G or tap
+      if nic_exists(opt.intf_clean) then -- 10G
+         config.app(c, "clean", Intel82599, {
+            pciaddr=opt.intf_clean
+         })
+
+         -- VLAN tagging on egress?
+         if opt.vlan_tag then
+            config.app(c, "vlan_tagger", vlan.Tagger, { tag = opt.vlan_tag })
+            -- link ddos -> vlan -> clean
+            config.link(c, "ddos.output -> vlan_tagger.input")
+            config.link(c, "vlan_tagger.output -> clean.rx")
+         else
+            -- link ddos -> clean
+            config.link(c, "ddos.output -> clean.rx")
+         end
+      else -- tap
+         print("clean interface '" .. opt.intf_clean .. "' is not an existing PCI device, assuming tap interface")
+         config.app(c, "clean", Tap, opt.intf_clean)
+
+         -- VLAN tagging on egress?
+         if opt.vlan_tag then
+            config.app(c, "vlan_tagger", vlan.Tagger, { tag = opt.vlan_tag })
+            -- link ddos -> vlan -> clean
+            config.link(c, "ddos.output -> vlan_tagger.input")
+            config.link(c, "vlan_tagger.output -> clean.input")
+         else
+            -- link ddos -> clean
+            config.link(c, "ddos.output -> clean.input")
+         end
+      end
    end
 
    -- report every second
@@ -105,19 +197,6 @@ function run (args)
       ))
    end
 
-   -- TODO: we need the reverse path set up as well so we can reply to ARP
-   -- packets but first we need the ARP app
-   config.app(c, "ddos", ddos.DDoS, { config_file_path = opt.mconfig_file_path })
-   config.link(c, "dirty.output -> ddos.input")
-   -- if vlan_tag is set we tag all egress/clean packets with a VLAN tag
-   if opt.vlan_tag then
-      print("Using VLAN tag: " .. opt.vlan_tag)
-      config.app(c, "vlan_tagger", vlan.Tagger, { tag = opt.vlan_tag })
-      config.link(c, "ddos.output -> vlan_tagger.input")
-      config.link(c, "vlan_tagger.output -> clean.input")
-   else
-      config.link(c, "ddos.output -> clean.input")
-   end
    engine.configure(c)
    engine.main()
 end
