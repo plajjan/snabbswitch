4b43f878f67057d2d7e21998682ead281d1e570f
Kristian Larsson <kristian@spritelink.net>
2016-04-08 21:42:57 +0200

    Add VlanMux

--- src/apps/vlan/README.md
+++ src/apps/vlan/README.md
@@ -1,26 +1,42 @@
 # VLAN
 
-There are two apps, Tagger and Untagger. As the name suggests one is used to
-add a VLAN tag to a packet whereas the other is used to strip one.
+There are four VLAN related apps, Tagger, Untagger and VlanMux. Tagger and
+Untagger are simple apps that add or remove a tag whereas VlanMux can mux and
+demux packets to different interfaces based on tag.
 
 ## Tagger
 
-Tagger adds a VLAN tag to packets received on the input interface and sends
-them to the output interface.
+Tagger adds a VLAN tag, with the configured value, to packets received on the
+input interface and sends them to the output interface.
 
 ### Configuration
 
 -  Key **tag**
 
 *Required*. VLAN tag to add or remove from the packet
+
 
 ## Untagger
 
-Untagger remove a VLAN tag from packets received on the input interface and
-sends them to the output interface.
+Untagger checks packets received on the input interface for a VLAN tag, removes
+it if it matches with the configured VLAN tag and sends them to the output
+interface. Packets with other VLAN tags than the configured tag will be dropped.
 
 ### Configuration
 
 -  Key **tag**
 
 *Required*. VLAN tag to add or remove from the packet
+
+
+## VlanMux
+
+VlanMux receives packets on the input interface named "trunk" and inspects
+the ethertype and VLAN tag field of received Ethernet frames. Frames with
+ethertype 0x8100 are inspected for the VLAN tag and sent out interface "vlanX"
+where X is the VLAN tag parsed from the packet. If no such output interface
+exists the frame is dropped. Received frames with an ethertype other than
+0x8100 are sent out the output interface "native".
+
+There is no configuration for VlanMux, simply link it to your other apps and it
+will base its actions on the name of the links.
--- src/apps/vlan/vlan.lua
+++ src/apps/vlan/vlan.lua
@@ -16,13 +16,31 @@ local dotq_tpid = 0x8100
 local o_ethernet_ethertype = 12
 local uint32_ptr_t = ffi.typeof('uint32_t*')
 
-local function make_vlan_tag(tag)
-   return ffi.C.htonl(bit.bor(bit.lshift(dotq_tpid, 16), tag))
+local function build_tci(vid)
+   return ffi.C.htonl(bit.bor(bit.lshift(dotq_tpid, 16), vid))
+end
+
+-- pop a VLAN tag from a packet
+function pop_tag(pkt)
+   local payload = pkt.data + o_ethernet_ethertype
+   local length = pkt.length
+   pkt.length = length - 4
+   C.memmove(payload, payload + 4, length - o_ethernet_ethertype - 4)
+end
+
+-- push a VLAN tag
+function push_tag(pkt, vid)
+   local tci = build_tci(vid)
+   local payload = pkt.data + o_ethernet_ethertype
+   local length = pkt.length
+   pkt.length = length + 4
+   C.memmove(payload + 4, payload, length - o_ethernet_ethertype)
+   cast(uint32_ptr_t, payload)[0] = tci
 end
 
 function Tagger:new(conf)
    local o = setmetatable({}, {__index=Tagger})
-   o.tag = make_vlan_tag(assert(conf.tag))
+   o.tag = build_tci(assert(conf.tag))
    return o
 end
 
@@ -42,7 +60,7 @@ end
 
 function Untagger:new(conf)
    local o = setmetatable({}, {__index=Untagger})
-   o.tag = make_vlan_tag(assert(conf.tag))
+   o.tag = build_tci(assert(conf.tag))
    return o
 end
 
@@ -56,10 +74,89 @@ function Untagger:push ()
          -- Incorrect VLAN tag; drop.
          packet.free(pkt)
       else
-         local length = pkt.length
-         pkt.length = length - 4
-         C.memmove(payload, payload + 4, length - o_ethernet_ethertype - 4)
+         pop_tag(p)
          transmit(output, pkt)
       end
    end
+end
+
+
+VlanMux = {}
+function VlanMux:new(conf)
+   local self = setmetatable({}, {__index=VlanMux})
+   self.ethertype_8100 = ffi.cast("uint16_t", 0x0081) -- 0x8100 in network byte order
+   return self
+end
+
+function VlanMux:push()
+   local noutputs = #self.output
+   if noutputs > 0 then
+      for name, l in pairs(self.input) do
+         local maxoutput = link.max
+         -- find out max number of packets we can put out an interface
+         -- this is kind of bad because we limit ourselves by the interface with
+         -- the fullest queue, yet packets might go out a different interface. We
+         -- don't know until we've looked in the packet and parsed the VLAN id. I
+         -- suppose we kind of get some HOLB with this :(
+         for _, o in ipairs(self.output) do
+            maxoutput = math.min(maxoutput, link.nwritable(o))
+         end
+
+         if type(name) == "string" then
+            for _ = 1, math.min(link.nreadable(l), maxoutput) do
+               local p = receive(l)
+               local ethertype = ffi.cast("uint16_t*", packet.data(p) + o_ethernet_ethertype)[0]
+
+               if name == "trunk" then -- trunk
+                  -- check for ethertype 0x8100 (802.1q VLAN tag)
+                  if ethertype == self.ethertype_8100 then
+                     -- dig out TCI field
+                     local tci = ffi.C.ntohs(ffi.cast("uint16_t*", packet.data(p) + o_ethernet_ethertype + 2)[0])
+                     -- extract VID from TCI
+                     local vid = bit.band(tci, 0xFFF)
+
+                     local oif = self.output["vlan"..vid]
+                     pop_tag(p)
+                     self:transmit(self.output.native, p)
+
+                  else -- untagged, send to native output
+                     self:transmit(self.output.native, p)
+                  end
+               else -- some vlanX interface
+                  local vid = tonumber(string.sub(name, 5))
+                  push_tag(p, vid)
+                  self:transmit(self.output.trunk, p)
+               end
+            end
+         end
+      end
+   end
+end
+
+-- transmit packet out interface if given interface exists, otherwise drop
+function VlanMux:transmit(o, pkt)
+   if o == nil then
+      packet.free(pkt)
+   else
+      transmit(o, pkt)
+   end
+end
+
+
+function selftest()
+   local app = require("core.app")
+   local basic_apps = require("apps.basic.basic_apps")
+
+   local c = config.new()
+   config.app(c, "source", basic_apps.Source)
+   config.app(c, "vlan_mux", VlanMux)
+   config.app(c, "sink", basic_apps.Sink)
+
+   config.link(c, "source.output -> vlan_mux.vlan1")
+   config.link(c, "vlan_mux.trunk -> sink.input")
+   app.configure(c)
+   app.main({duration = 1})
+
+   print("source sent: " .. link.stats(app.app_table.source.output.output).txpackets)
+   print("sink received: " .. link.stats(app.app_table.sink.input.input).rxpackets)
 end
